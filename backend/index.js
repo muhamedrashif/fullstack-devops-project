226204016069b5d99f0b74905c9e291424db0d1f
Ubuntu <ubuntu@ip-172-31-6-61.ap-south-1.compute.internal>
2025-06-10 04:41:37 +0000

    Trigger webhook test

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
-app.get('/api', (req, res) => res.send('API Working!'));
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+// Add this root route
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
